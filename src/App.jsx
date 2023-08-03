1e0fef4a8474e53979b7877bceadb4e7da1d2ab2
26th-Official <you@example.com>
2023-08-03 19:31:20 +0530

    Tests for WillDo Project

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useState,useEffect } from 'react'
+import axios from 'axios'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    axios.post("http://localhost:6565/signin",{
+      "Username": "test",
+      "Password": "Abc@1234"
+    }).then(res => console.warn(res))
+
+    axios.get("http://localhost:6565/getall",{
+      withCredentials : true
+    }).then(res => console.warn(res))
+  })
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
